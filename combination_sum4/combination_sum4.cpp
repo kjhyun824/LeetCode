838d8f6f8118a5c59e5d819e3cb6db4cdf8ebb97
JungHyun <woori4829@unist.ac.kr>
2021-04-07 03:02:05 +0900

    377 - Combination Sum - Success

---
+++ combination_sum4/combination_sum4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int combinationSum4(vector<int>& nums, int target) {
+	sort(nums.begin(), nums.end());
+	vector<int> count_map(target + 1, 0);
+	count_map[0] = 1;
+
+	for(int i = 0; i < count_map.size(); i++) {
+		for(int j = 0; j < nums.size() && nums[j] <= i; j++) {
+			count_map[i] += count_map[i - nums[j]];
+		}
+	}
+
+	return count_map[target];
+}
+
+int main() {
+	int T;
+	cin >> T;
+
+	for(int i = 0; i < T; i++) {
+		int num;
+		vector<int> nums;
+		int target;
+
+		cin >> num;
+		for(int j = 0; j < num; j++) {
+			int temp;
+			cin >> temp;
+			nums.push_back(temp);
+		}
+		cin >> target;
+
+		int output = combinationSum4(nums, target);
+
+		cout << "Case #" << i+1 << endl;
+		cout << output << endl;
+	}
+
+	return 0;
+}
